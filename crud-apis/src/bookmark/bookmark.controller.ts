66bd367c6bd18c5f4490218ba2bfa9927eedc62e
psiwo1f <>
2022-08-22 18:21:50 +0500

    doc: openApi swagger

--- crud-apis/src/bookmark/bookmark.controller.ts
+++ crud-apis/src/bookmark/bookmark.controller.ts
@@ -1,34 +1,42 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, ParseIntPipe, Patch, Post, UseGuards, HttpStatus } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { GetUser } from '../auth/decorator';
 import { JwtGuard } from '../auth/guard';
 import { BookmarkService } from './bookmark.service';
 import { CreateBookmarkDto, EditBookmarkDto } from './dto';
 
-
+@ApiBearerAuth()
+@ApiTags('bookmarks')
 @UseGuards(JwtGuard)
 @Controller('bookmarks')
 export class BookmarkController {
 	constructor(private bookmarkService: BookmarkService) {}
+	@ApiOperation ({ summary: 'Get all bookmarks' })
+	// @ApiResponse({ status: 403, description: 'Forbidden.' })
 	@Get()
 	getBookmarks(@GetUser('id') userId: number) {
 		return this.bookmarkService.getBookmarks(userId)
 	}
 
+	@ApiOperation ({ summary: 'Get a bookmark' })
 	@Get(':id')
 	getBookmarkById(@GetUser('id') userId: number, @Param('id', ParseIntPipe) bookmarkId: number) {
 		return this.bookmarkService.getBookmarkById(userId, bookmarkId)
 	}
 
+	@ApiOperation ({ summary: 'Create a bookmark' })
 	@Post()
 	createBookmark(@GetUser('id') userId: number, @Body() dto: CreateBookmarkDto) {
 		return this.bookmarkService.createBookmark(userId, dto)
 	}
 
+	@ApiOperation ({ summary: 'Edit a bookmark' })
 	@Patch(':id')
 	editBookmarkById(@GetUser('id') userId: number, @Param('id', ParseIntPipe) bookmarkId: number, @Body() dto: EditBookmarkDto) {
 		return this.bookmarkService.editBookmarkById(userId, bookmarkId, dto)
 	}
 
+	@ApiOperation ({ summary: 'Delete a bookmark' })
 	@HttpCode(HttpStatus.NO_CONTENT)
 	@Delete(':id')
 	deleteBookmarkById(@GetUser('id') userId: number, @Param('id', ParseIntPipe) bookmarkId: number) {
--- crud-apis/src/main.ts
+++ crud-apis/src/main.ts
@@ -1,9 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule,DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('CRUD Apis')
+    // .setDescription('The cats API description')
+    .setVersion('1.0')
+    // .addTag('cats')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
   }))
