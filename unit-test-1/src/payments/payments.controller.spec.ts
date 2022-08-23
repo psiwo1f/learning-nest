44202ee97cf2d916b8c3ffabf1ebe170d4108aaa
psiwo1f <>
2022-08-23 12:33:42 +0500

    unit test controller

--- unit-test-1/src/payments/payments.controller.spec.ts
+++ unit-test-1/src/payments/payments.controller.spec.ts
@@ -1,8 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Request, Response } from 'express';
 import { PaymentsController } from './payments.controller';
 
 describe('PaymentsController', () => {
   let controller: PaymentsController;
+  const reqMock = {
+    query: {},
+  } as unknown as Request
+
+  const statusResMock = {
+    send: jest.fn(x => x)
+  }
+  const respMock = {
+    status: jest.fn(x => statusResMock),
+    send: jest.fn(x => x)
+  } as unknown as Response
+  // const respMock = {
+  //   status: jest.fn(x => ({
+  //     send: jest.fn(y => y)
+  //   })),
+  //   send: jest.fn(x => x)
+  // } as unknown as Response
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -15,4 +33,20 @@ describe('PaymentsController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe('allpayments', () => {
+    it('should return 400 status without query params', () => {
+      controller.allPayments(reqMock, respMock)
+      expect(respMock.status).toHaveBeenCalledWith(400)
+      expect(statusResMock.send).toHaveBeenCalledWith({msg: 'Query params missing'})
+    })
+    it('should return 200 status with query params', () => {
+      reqMock.query = {
+        count: '20',
+        page: '1'
+      }
+      controller.allPayments(reqMock, respMock)
+      expect(respMock.send).toHaveBeenCalledWith(200)
+    })
+  })
 });
--- unit-test-1/src/payments/payments.controller.ts
+++ unit-test-1/src/payments/payments.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Controller('payments')
-export class PaymentsController {}
+export class PaymentsController {
+  @Get()
+  allPayments(@Req() req: Request, @Res() res: Response) {
+    const { count, page } = req.query;
+    if (!(count || page)) {
+      res.status(400).send({ msg: 'Query params missing' });
+    } else {
+      res.send(200);
+    }
+  }
+}
