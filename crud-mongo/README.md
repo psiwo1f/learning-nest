f4b4773770f2c41112558e7195bc282e9a345917
psiwo1f <>
2022-09-01 16:31:34 +0500

    crud with mongo

---
+++ crud-mongo/README.md
@@ -0,0 +1,9 @@
+
+
+## Tools
+- **Express MongoDB:** is an interactive lightweight Web-Based Administrative Tool to effectively manage MongoDB Databases 
+ in docker-compose file mongo is database, mongo-express is GUI to view db in browser
+
+
+## Notes
+ - **Nullish coalescing operator (??)** is a logical operator that returns its right-hand side operand when its left-hand side operand is null or undefined, and otherwise returns its left-hand side operand.
---
+++ crud-mongo/api/src/product/product.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { ProductDocument } from './product.schema';
+
+@Injectable()
+export class ProductService {
+  constructor(
+    @InjectModel('Product')
+    private readonly productModel: Model<ProductDocument>,
+  ) {}
+
+  async create(
+    name: string,
+    price: number,
+    description: string,
+  ): Promise<ProductDocument> {
+    const newProduct = new this.productModel({ name, price, description });
+    return newProduct.save();
+  }
+
+  async findAll(): Promise<ProductDocument[]> {
+    return this.productModel.find().exec(); // good to use exec() with fetching queries, read more on that
+  }
+
+  async find(id: string): Promise<ProductDocument> {
+    return this.productModel.findById(id).exec();
+  }
+
+  async update(id: string, name: string, price: number, description: string) {
+    const existingProduct = await this.productModel.findById(id).exec()
+
+    existingProduct.name = name ?? existingProduct.name
+    existingProduct.price = price ?? existingProduct.price
+    existingProduct.description = description ?? existingProduct.description
+    return existingProduct.save()
+  }
+
+  async delete(id: string) {
+    return this.productModel.deleteOne({_id:id}).exec()
+  }
+}
