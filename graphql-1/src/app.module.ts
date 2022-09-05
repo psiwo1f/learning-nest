a0d8a65549332253a2d568847340945c82744fc3
Syed Haider <>
2022-09-05 12:18:08 +0500

    graphql setup

--- graphql-1/src/app.module.ts
+++ graphql-1/src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthorModule } from './author/author.module';
 import { BookModule } from './book/book.module';
-
+import { ApolloDriver, ApolloDriverConfig} from '@nestjs/apollo'
 @Module({
-  imports: [AuthorModule, BookModule],
+  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
+    autoSchemaFile: 'schema.gql',
+  }), 
+  AuthorModule, BookModule],
   controllers: [AppController],
   providers: [AppService],
 })
